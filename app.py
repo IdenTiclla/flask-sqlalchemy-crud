a0304b09c14b0c566dcfe59461a133bbc99c4998
IdenTiclla <iden.ticlla@gmail.com>
2021-11-26 17:35:27 -0400

    Variables de entorno

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from routes.contacts import contacts
 from flask_sqlalchemy import SQLAlchemy
-
+from config import DATABASE_CONNECTION_URI
 app = Flask(__name__)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root@localhost/contactsdb"
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 SQLAlchemy(app)
